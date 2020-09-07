c4995a444203e4f9429b44136dc5444e99baec22
Yoju Lee <yoyoyoju@gmail.com>
2020-09-07 13:35:36 +0200

    Re-Implement repository_core

--- loop_record_repository_impl/lib/src/key_value_storage.dart
+++ loop_record_repository_impl/lib/src/key_value_storage.dart
@@ -3,8 +3,7 @@ import 'dart:convert';
 import 'package:key_value_store/key_value_store.dart';
 import 'package:loop_record_repository_core/loop_record_repository_core.dart';
 
-const AUDIO_SETTINGS_KEY = 'audio_settings';
-const THEME_SETTINGS_KEY = 'theme_settings';
+const SETTINGS_KEY = 'settings';
 
 /// Loads and saves Settings using a provided KeyValueStore, which works
 /// on mobile and web. On mobile, it uses the SharedPreferences package,
@@ -17,48 +16,18 @@ class KeyValueStorage implements SettingsRepository {
   const KeyValueStorage(this.key, this.store, [this.codec = json]);
 
   @override
-  Future<AudioSettingsEntity> loadAudioSettings() async {
-    final map = codec.decode(store.getString(key))[AUDIO_SETTINGS_KEY];
-    return AudioSettingsEntity.fromJson(map);
-  }
-
-  @override
-  Future<bool> saveAudioSettings(AudioSettingsEntity settings) {
-    return store.setString(
-      key,
-      codec.encode({
-        AUDIO_SETTINGS_KEY: settings.toJson(),
-      }),
-    );
-  }
-
-  @override
-  Future<ThemeSettingsEntity> loadThemeSettings() async {
-    final map = codec.decode(store.getString(key))[THEME_SETTINGS_KEY];
-    return ThemeSettingsEntity.fromJson(map);
-  }
-
-  @override
-  Future<bool> saveThemeSettings(ThemeSettingsEntity settings) {
-    return store.setString(
-      key,
-      codec.encode({
-        THEME_SETTINGS_KEY: settings.toJson(),
-      }),
-    );
-  }
-
-  @override
   Future<SettingsEntity> loadSettings() async {
-    final audioSettingsEntity = loadAudioSettings();
-    final themeSettingsEntity = loadThemeSettings();
-    return SettingsEntity(await audioSettingsEntity, await themeSettingsEntity);
+    final map = codec.decode(store.getString(key))[SETTINGS_KEY];
+    return SettingsEntity.fromJson(map);
   }
 
   @override
   Future<bool> saveSettings(SettingsEntity settings) async {
-    final saveAudio = saveAudioSettings(settings.audioSettingsEntity);
-    final saveTheme = saveThemeSettings(settings.themeSettingsEntity);
-    return (await saveAudio) && (await saveTheme);
+    return store.setString(
+      key,
+      codec.encode({
+        SETTINGS_KEY: settings.toJson(),
+      }),
+    );
   }
 }
--- loop_record_repository_impl/test/key_value_storage_test.dart
+++ loop_record_repository_impl/test/key_value_storage_test.dart
@@ -11,77 +11,24 @@ class MockKeyValueStore extends Mock implements KeyValueStore {}
 void main() {
   group('KeyValueStorage for Audio', () {
     final store = MockKeyValueStore();
-    final audioSettings = AudioSettingsEntity(true, 1.0, 1.0);
-    final audioSettingsJson =
-        '{"$AUDIO_SETTINGS_KEY":{"toLoop":true,"volumn":1.0,"playbackRate":1.0}}';
-    final storage = KeyValueStorage('T', store);
-
-    test('Should persist SettingsEntities to the store', () async {
-      // Check the settingsJson is okay
-      expect(
-          json.encode({
-            AUDIO_SETTINGS_KEY: audioSettings.toJson(),
-          }),
-          audioSettingsJson);
-      await storage.saveAudioSettings(audioSettings);
-      verify(store.setString('T', audioSettingsJson));
-    });
-
-    test('Should load SettingsEntity from disk', () async {
-      when(store.getString('T')).thenReturn(audioSettingsJson);
-      expect(await storage.loadAudioSettings(), audioSettings);
-    });
-  });
-
-  group('KeyValueStorage for Theme', () {
-    final store = MockKeyValueStore();
-    final themeSettings = ThemeSettingsEntity(false);
-    final themeSettingsJson = '{"$THEME_SETTINGS_KEY":{"isDarkMode":false}}';
-    final storage = KeyValueStorage('T', store);
-
-    test('Should persist SettingsEntities to the store', () async {
-      // Check the settingsJson is okay
-      expect(
-          json.encode({
-            THEME_SETTINGS_KEY: themeSettings.toJson(),
-          }),
-          themeSettingsJson);
-      await storage.saveThemeSettings(themeSettings);
-      verify(store.setString('T', themeSettingsJson));
-    });
-
-    test('Should load SettingsEntity from disk', () async {
-      when(store.getString('T')).thenReturn(themeSettingsJson);
-      expect(await storage.loadThemeSettings(), themeSettings);
-    });
-  });
-
-  group('KeyValueStorage', () {
-    final store = MockKeyValueStore();
-    final themeSettings = ThemeSettingsEntity(false);
-    final themeSettingsJson = '{"$THEME_SETTINGS_KEY":{"isDarkMode":false}}';
-    final audioSettings = AudioSettingsEntity(true, 1.0, 1.0);
-    final audioSettingsJson =
-        '{"$AUDIO_SETTINGS_KEY":{"toLoop":true,"volumn":1.0,"playbackRate":1.0}}';
-    final settings = SettingsEntity(audioSettings, themeSettings);
+    final settings = SettingsEntity(true, 1.0, 1.0, false);
     final settingsJson =
-        '{"$AUDIO_SETTINGS_KEY":{"toLoop":true,"volumn":1.0,"playbackRate":1.0},"$THEME_SETTINGS_KEY":{"isDarkMode":false}}';
+        '{"$SETTINGS_KEY":{"toLoop":true,"volumn":1.0,"playbackRate":1.0,"isDarkMode":false}}';
     final storage = KeyValueStorage('T', store);
 
     test('Should persist SettingsEntities to the store', () async {
       // Check the settingsJson is okay
       expect(
           json.encode({
-            THEME_SETTINGS_KEY: themeSettings.toJson(),
+            SETTINGS_KEY: settings.toJson(),
           }),
-          themeSettingsJson);
+          settingsJson);
       await storage.saveSettings(settings);
-      verify(store.setString('T', themeSettingsJson));
-      verify(store.setString('T', audioSettingsJson));
+      verify(store.setString('T', settingsJson));
     });
 
     test('Should load SettingsEntity from disk', () async {
-      when(store.getString('T')).thenReturn(themeSettingsJson);
+      when(store.getString('T')).thenReturn(settingsJson);
       expect(await storage.loadSettings(), settings);
     });
   });
