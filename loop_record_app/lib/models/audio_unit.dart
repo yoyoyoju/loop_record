8d9d5b8f019a4991c82e891ed71624abe6dade8b
Yoju Lee <yoyoyoju@gmail.com>
2020-09-08 13:54:35 +0200

    Add init for audio unit

--- loop_record_app/lib/models/audio_unit.dart
+++ loop_record_app/lib/models/audio_unit.dart
@@ -1,15 +1,47 @@
+import 'dart:io' as io;
+
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter_audio_recorder/flutter_audio_recorder.dart';
 import 'package:loop_record_app/models/enums.dart';
+import 'package:path_provider/path_provider.dart';
 
 class AudioUnitImpl implements AudioUnit {
   AudioPlayer _audioPlayer;
   FlutterAudioRecorder _recorder;
+  Recording _currentRecording;
+  RecordingStatus _currentStatus;
+//  PlayerState _playerState;
 
   @override
   Future<AudioUnitHealth> init() async {
     try {
       if (await FlutterAudioRecorder.hasPermissions) {
+        String customPath = '/loop_record_app';
+        io.Directory appDocDirectory;
+        if (io.Platform.isIOS) {
+          appDocDirectory = await getApplicationDocumentsDirectory();
+        } else {
+          appDocDirectory = await getExternalStorageDirectory();
+        }
+
+        customPath = appDocDirectory.path + customPath + ".wav";
+        await _delete(customPath);
+
+        // .wav <---> AudioFormat.WAV
+        // .mp4 .m4a .aac <---> AudioFormat.AAC
+        // AudioFormat is optional, if given value, will overwrite path extension when there is conflicts.
+        _recorder =
+            FlutterAudioRecorder(customPath, audioFormat: AudioFormat.WAV);
+
+        await _recorder.initialized;
+        // after initialization
+        var current = await _recorder.current(channel: 0);
+        print(current);
+        // should be "Initialized", if all working fine
+
+        _currentRecording = current;
+        _currentStatus = current.status;
+
         return AudioUnitHealth.ok;
       } else {
         return AudioUnitHealth.needPermissions;
@@ -49,6 +81,8 @@ class AudioUnitImpl implements AudioUnit {
     }
     return false;
   }
+
+  void _delete(String customPath) async {}
 }
 
 abstract class AudioUnit {
